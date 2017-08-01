0e4b0772fe8fdfa157cf9da0827748bef990fa54
bobsira <sbobfitz2@gmail.com>
2017-08-01 18:27:37 +0300

    Implemented release method on all the screens

--- app/src/main/java/com/example/android/miwok/ColorsActivity.java
+++ app/src/main/java/com/example/android/miwok/ColorsActivity.java
@@ -79,6 +79,10 @@ public class ColorsActivity extends AppCompatActivity {
 
                 // Start the audio file
                 mMediaPlayer.start();
+
+                // Setup a listener on the media player, so that we can stop and release the
+                // media player once the sound has finished playing.
+                mMediaPlayer.setOnCompletionListener(mCompletionListener);
             }
         });
     }
